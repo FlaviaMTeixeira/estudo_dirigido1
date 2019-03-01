d996fc5a00a7213cd28790a6f76e1d3f7836dcf4
FlaviaMTeixeira <48135683+FlaviaMTeixeira@users.noreply.github.com>
2019-03-01 16:04:29 -0300

    Adicionando arquivo do pc

---
+++ Exercicio_Cigarro.php
@@ -0,0 +1,15 @@
+<?php
+
+    print "Quantos cigarros você fuma por dia: ";
+    $cigarros_por_dia = fgets(STDIN);
+
+    print "Quantos anos você fuma: ";
+    $anos_fumados = fgets(STDIN);
+
+    $cigarros_fumados = 10*365*3;
+    $minutos_perdidos = $cigarros_fumados * 11;
+    $horas_perdidas   = $minutos_perdidos /60;
+    $dias_perdidos    = round($horas_perdidas/24);
+
+    
+    print "Você perdeu $dias_perdidos dias de vida! \n" ;
